210cab37392c39d7e9fffebe36182b784d17ef7e
fanderkai <felixanderkai@gmail.com>
2026-04-15 20:48:16 +0800

    2nd fix attempt

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -7,20 +7,27 @@ sns.set(style='darkgrid')
 from datetime import timedelta
 import os
 
-def create_monthly_orders_df(all_df):
-    all_df['order_approved_at'] = pd.to_datetime(
-        all_df['order_approved_at'],
-        errors='coerce'
-    )
+def create_monthly_orders_df(df):
 
-    all_df = all_df.dropna(subset=['order_approved_at'])
-
-    monthly_orders_df = all_df.resample(
-        rule='MS',
-        on='order_approved_at'
-    ).agg({
-        'order_id': 'count'
+    monthly_orders_df = df.resample(rule='MS', on='order_approved_at').agg({
+        "order_id": "nunique",
+        "payment_value": "sum",
     })
+
+    monthly_orders_df = monthly_orders_df.reset_index()
+
+    monthly_orders_df.rename(columns={
+        "order_approved_at": "order_month",
+        "order_id": "order_count",
+        "payment_value": "payment"
+    }, inplace=True)
+
+    recent_month = monthly_orders_df['order_month'].max().replace(day=1)
+    monthly_orders_df = monthly_orders_df[
+        monthly_orders_df['order_month'] < recent_month
+    ]
+
+    monthly_orders_df = monthly_orders_df.sort_values('order_month')
 
     return monthly_orders_df
 
